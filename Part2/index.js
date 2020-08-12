6b3c4f98df0ddac8e1fa22f6e65b8c795debb106
Kyle Hansen <1hansen.kyle@gmail.com>
2020-08-11 23:44:59 -0600

    vanilla js afternoon project

---
+++ Part2/index.js
@@ -0,0 +1,14 @@
+console.log('The house always wins!')
+
+const setId = document.getElementById('idInput')
+
+const setColor = document.getElementById('colorInput')
+
+// console.log(setId)
+// console.log(setColor)
+
+function setCard() {
+    const card = document.getElementById(setId.value)
+    // console.log(card) didn't see the console log?
+    card.style.color = setColor.value
+}
